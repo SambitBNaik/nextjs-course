732d9bf585e2ed067cd49aef42063faa813a2988
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-01-17 11:29:06 +0530

    first-commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    console.log("About page");
+  return (
+    <div>
+        <h1>Hello from about page.</h1>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import CreatePost from "@/components/CreatePost";
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import WhoToFollow from "@/components/WhoToFollow";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export default  async function Home() {
+  const user= await currentUser();
+  return (
+  <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+    <div className="lg:col-span-6">
+      {user? <CreatePost/>: null}
+    </div>
+    <div className="hidden lg:block lg:col-span-4 sticky top-20">
+      <WhoToFollow/>
+    </div>
+  </div>
+  );
+}
